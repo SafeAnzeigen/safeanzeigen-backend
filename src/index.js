b431beb1e4eaed92077e9df53663be1a87a38b95
Sascha Majewsky <saschamaj@protonmail.com>
2022-06-03 15:02:49 +0200

    Socket on message testing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 require('dotenv').config();
 
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://safeanzeigen.de',
+  'https://www.safeanzeigen.de',
+];
+
 const server = require('./server');
 const db = require('./database/db');
+const server2 = require('http').createServer(server);
+const io = require('socket.io')(server2, { cors: { origin: whitelist } });
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 's.majewsky93@gmail.com', // Change to your recipient
-  from: process.env.SENDGRID_VERIFIED_SENDER_EMAIL, // Change to your verified sender
-  subject: 'My first mail',
-  text: 'my mail',
-  html: '<strong>This is my mail</strong>',
-};
+io.on('connection', (socket) => {
+  const id = socket.handshake.query.id;
+  console.log('handshake connection from clerk user', socket.handshake.query.id);
+  socket.join(id);
 
-server.listen(process.env.PORT || 5000, () =>
+  socket.on('message', (messageObject) => {
+    console.log('messageObject', messageObject);
+    socket.emit('receive-message', messageObject);
+  });
+
+  socket.on('join', (messageObject) => {
+    console.log('joinObject', messageObject);
+  });
+
+  socket.on('is-typing', ({ isTypingObject }) => {
+    console.log('isTypingObject', isTypingObject);
+  });
+});
+
+server2.listen(process.env.PORT || 5000, () =>
   db
     .raw('select 1')
     .then(() => {
@@ -21,14 +39,6 @@ server.listen(process.env.PORT || 5000, () =>
       console.log(
         `\n *** safeanzeigen API server ${process.env.DEPLOYMENT} environment running on ${process.env.SERVER_URL} ***\n`
       );
-      /*  sgMail
-        .send(msg)
-        .then(() => {
-          console.log('Email sent');
-        })
-        .catch((error) => {
-          console.error(error);
-        }); */
     })
     .catch((error) => {
       console.log('Connection to database failed. ', error);
