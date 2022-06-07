cf8894e537c20bd6bad4aa508cb823a3cfa0b4bf
Sascha Majewsky <saschamaj@protonmail.com>
2022-06-07 12:08:53 +0200

    Secured chats and messages routes

--- src/api/resources/messages/messages-router.js
+++ src/api/resources/messages/messages-router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const authorizationMiddleware = require('../../middlewares/authorization');
 const controller = require('./messages-controller');
 
-/* TODO: PROTECT GET  WRITE VALIDATION WHERE CLERK_USER_ID ASKING FOR MESSAGES HAS TO BE ROOM_CREATOR OF CHATROOM (BUYER) OR HAS TO OWN THE AD_ID OF CHATROOM */
-
-router.get('/:ad_conversation_room_id', controller.getAllMessagesByAdConversationRoomId);
+router.get(
+  '/:ad_conversation_room_id',
+  authorizationMiddleware.validateAuthorization,
+  controller.getAllMessagesByAdConversationRoomId
+);
 
 // NOT USED
 /* router.post('/', controller.addMessage); */
